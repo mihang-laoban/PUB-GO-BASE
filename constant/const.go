1d1e5edf30177a0bf05ed4fc0c2d1d63f03b10f8
tar <mihanglaoban@outlook.com>
2021-06-29 00:32:49 +0800

    add unix time tools

---
+++ constant/const.go
@@ -0,0 +1,6 @@
+package constant
+
+var (
+	STD_TIME_FORMAT = "2006-01-02 15:04:05"
+)
+
--- utils/tools.go
+++ utils/tools.go
@@ -1,10 +1,20 @@
 package utils
 
 import (
-	"fmt"
+	"github.com/mihang-laoban/PUB-GO-BASE/constant"
 	"time"
 )
 
-func GetNow()  {
-	fmt.Println(time.Now())
+
+func Time2Unix(timeStr string) int64 {
+	stamp, _ := time.ParseInLocation(constant.STD_TIME_FORMAT, timeStr, time.Local)
+	return stamp.Unix()
+}
+
+func Unix2Time(unixTime int64) string {
+	return time.Unix(unixTime, 0).Format(constant.STD_TIME_FORMAT)
+}
+
+func GetNowStr() string {
+	return Unix2Time(time.Now().Unix())
 }
